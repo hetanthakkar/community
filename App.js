db161d92ddc50633f24f3e7e7d98c472f1a1b26a
Hetan Thakkar <hetanthakkar1@gmail.com>
2022-04-20 21:48:18 +0530

    fixed dependencies

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import {Text, View} from 'react-native';
-import React, {Component} from 'react';
-import MapView from 'react-native-maps';
+import React, { useState } from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <MapView
-          style={{flex: 1}}
-          initialRegion={{
-            latitude: 27.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
-      </View>
-    );
-  }
+import { View,Text } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import DropDownPicker from 'react-native-dropdown-picker';
+import ImagePicker from 'react-native-image-crop-picker';
+import { MenuProvider } from 'react-native-popup-menu';
+import {
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+
+} from 'react-native-popup-menu';
+import { ToastProvider,useToast } from 'react-native-toast-notifications'
+import Icon from 'react-native-vector-icons/FontAwesome'
+const App=()=> {
+  const toast = useToast();
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(['italy', 'spain', 'barcelona', 'finland']);
+  const [items, setItems] = useState([
+    {label: 'Spain', value: 'spain'},
+    {label: 'Madrid', value: 'madrid', parent: 'spain'},
+    {label: 'Barcelona', value: 'barcelona', parent: 'spain'},
+
+    {label: 'Italy', value: 'italy'},
+    {label: 'Rome', value: 'rome', parent: 'italy'},
+
+    {label: 'Finland', value: 'finland'}
+  ]);
+
+  return (
+    <View style={{
+      // backgroundColor: '#171717',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingHorizontal: 15
+    }}>
+<Text onPress={()=>toast.show("Hello World")}>skdnfoisndf</Text>
+<Icon name="phone" size={20}/>
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+
+        theme="DARK"
+        multiple={true}
+        mode="BADGE"
+        badgeDotColors={["#e76f51", "#00b4d8", "#e9c46a", "#e76f51", "#8ac926", "#00b4d8", "#e9c46a"]}
+      />
+<Menu>
+      <MenuTrigger text='Select action' />
+      <MenuOptions>
+        <MenuOption onSelect={() => alert(`Save`)} text='Save' />
+        <MenuOption onSelect={() => alert(`Delete`)} >
+          <Text style={{color: 'red'}}>Delete</Text>
+        </MenuOption>
+        <MenuOption onSelect={() => alert(`Not called`)} disabled={true} text='Disabled' />
+      </MenuOptions>
+    </Menu>
+    </View>
+  );
 }
+ const App1= () => (
+  <MenuProvider>
+        <ToastProvider>
+
+    <App />
+    </ToastProvider>
+  </MenuProvider>
+);
+export default App1
